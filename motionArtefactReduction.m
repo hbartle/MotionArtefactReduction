48635a64cd2ce029960a5cbcb79d7bd469afd8ea
Hannes Bartle <hannes@michaelbartle.de>
2017-10-10 20:50:21 +0200

    Create Main file

---
+++ motionArtefactReduction.m
@@ -0,0 +1,13 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%   Motion Artefact Reduction Algorithm
+%
+%   Wearable Electronic Devices Course E17
+%   M.Sc. Electrical Engineering/Computer Engineering
+%   Aarhus University
+%   2017
+%
+%   Tongtong Jiang, Petr Kryze, Hannes Bartle
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
