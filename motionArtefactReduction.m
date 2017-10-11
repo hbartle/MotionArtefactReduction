e4272c14ff8892ad9659d4d63273b3dda88accf8
Hannes Bartle <hannes@michaelbartle.de>
2017-10-11 19:43:35 +0200

    Add First Draft of Main Script

--- motionArtefactReduction.m
+++ motionArtefactReduction.m
@@ -20,12 +20,66 @@ load('data/steady/sar1_test1.mat');
 % SAR2 (Microphone 2)
 load('data/steady/sar2_test1.mat');
 
+%% Parameter Definition
+
+% NLMS Filter Order
+filterOrder = 10;
+% NLMS Step Size
+stepSize = 0.1;
+
+% Power Estimation Weight Factor
+beta = 0.3;
+% Weight Adaption Sensitivity
+gamma = 0.1;
+
+%% Signal Initialization
 
 
 %% Processing Loop
 for i = 1:length(delsig)
    
+    % High Pass Filter to remove offset
+    xElectrode(i) = highPassFilter(delsig(i));
+    xMic1(i) = highPassFilter(sar1(i));
+    xMic2(i) = highPassFilter(sar2(i));
     
+    % Adaptive FIR
+    uMic1(i) = nlmsCoefficients1 * xMic1(i-filterOrder:i);
+    uMic2(i) = nlmsCoefficients2 * xMic2(i-filterOrder:i);
     
+    % Construct the error signal
+    error1(i) = xElectrode(i) - uMic1(i);
+    error2(i) = xElectrode(i) - uMic2(i);
     
+    % Update the Adaptive FIR Filter Coefficients
+    nlmsCoefficients1 = updateNLMS(nlmsCoefficients1,xMic1(i-filterOrder:i),...
+                                   error1(i),stepSize);
+    nlmsCoefficients2 = updateNLMS(nlmsCoefficients2,xMic2(i-filterOrder:i),...
+                                   error2(i),stepSize);
+
+    % Estimate Power of the Prediction Error
+    powerError1(i) = predictionErrorEstimation(powerError1(i-1),...
+                                               error1(i), beta);
+    powerError2(i) = predictionErrorEstimation(powerError2(i-1),...
+                                               error2(i), beta);
+    
+    % Update the Signal Combination Weight
+    alpha(i) = weightAdaption(alpha(i-1),powerError1(i),...
+                              powerError2(i),gamma);
+    
+    % Calculate the Output Signal
+    output(i) = alpha(i) * error1(i) + (1-alpha(i)) * error2(i);
 end
+
+
+%% Post Processing and Plotting
+inputSignalFigure = figure('NumberTitle','off','Name','Input Signals');
+subplot(3,1,1);
+plot(delsig);
+subplot(3,1,2);
+plot(sar1);
+subplot(3,1,3);
+plot(sar2);
+
+
+
