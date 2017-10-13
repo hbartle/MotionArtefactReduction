036acdbe05c510d7870d7e6b0d49bed7652c46fc
Hannes Bartle <hannes@michaelbartle.de>
2017-10-13 11:58:55 +0200

    Add clipping to alpha computation
    Alpha should be in the range between 0 and 1.

--- weightAdaption.m
+++ weightAdaption.m
@@ -3,7 +3,11 @@ function [ newAlpha ] = weightAdaption( oldAlpha, estimatedPower1,...
 %WEIGHTADAPTION Calculate the weight of each predicted signal using the
 %prediction error estimation.
 
-newAlpha = oldAlpha - gamma * sgn(estimatedPower1 -estimatedPower2);
-
+newAlpha = oldAlpha - gamma * sign(estimatedPower1 -estimatedPower2);
+if newAlpha > 1
+    newAlpha = 1;
+elseif newAlpha < 0
+    newAlpha = 0;
+end
 end
 
